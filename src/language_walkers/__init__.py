15bf3f156030895c1b8b5769a17cd6b32dd318f1
Antoine de Comite <53222045+decomiteA@users.noreply.github.com>
2023-03-05 15:39:23 -0500

    Update __init__.py

--- src/language_walkers/__init__.py
+++ src/language_walkers/__init__.py
@@ -4,5 +4,5 @@ from gymnasium.envs.registration import register
 register(
   id="bipedal_walker-v1",
   entry_point="language_walkers.envs:BipedalWalkerV1",
-  max_episode_steps="1000",
+  max_episode_steps=1000,
 )
